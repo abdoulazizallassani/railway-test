c7653ad70c9e2c0e17b4d80d494acc57222758d3
abdoulazizallassani <allassaniabdoulaziz@gmail.com>
2026-02-22 13:19:58 +0100

    Initial test

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ status: "Railway CI/CD works 🚀" }));
+}).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
