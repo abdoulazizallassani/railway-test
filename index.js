6ac98eb7a49ebf88626be7bab7d3700a08b65864
abdoulazizallassani <allassaniabdoulaziz@gmail.com>
2026-02-22 13:34:41 +0100

    Check CI/CD

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ status: "Railway CI/CD works 🚀" }));
+  res.end(JSON.stringify({ status: "Railway CI/CD works 🚀 ++ " }));
 }).listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
